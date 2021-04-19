70867d90abdf9b8920b7ba2bbe17f6068f2dd684
Walther Lee <walthere.lee@gmail.com>
2021-04-19 10:44:54 -0700

    refactor cmd to get multiple args

--- secrets.py
+++ secrets.py
@@ -1,17 +1,32 @@
 import base64
 import json
+import shlex
 import sys
+from subprocess import Popen, PIPE
 
 
 def main():
-    try:
-        obj = json.load(sys.stdin)
-    except ValueError:
-        return
-    for k, v in obj.get('data', {}).items():
-        obj['data'][k] = base64.b64decode(v.encode()).decode()
+    name = sys.argv[1]
+    with Popen(["kubectl", "get", "secrets", name, "-o=json"], stdout=PIPE) as pc:
+        current = json.load(pc.stdout)
 
-    print(json.dumps(obj, indent=4))
+    objs = [
+        (k, base64.b64decode(v.encode()).decode())
+        for k, v in current.get('data', {}).items()
+    ]
+    inp = input('Write new secret key-value (leave empty to finish): ')
+    while inp:
+        k, v = inp.split('=', 1)
+        objs.append((k, v))
+        inp = input('Write new secret key-value (leave empty to finish): ')
+
+    cmd = f"kubectl create secret generic {current['metadata']['name']} \\\n"
+    values = [
+        f"  --from-literal={k}={shlex.quote(v)} "
+        for k, v in objs
+    ]
+    cmd += '\\\n'.join(values)
+    print(cmd)
 
 
 if __name__ == '__main__':
